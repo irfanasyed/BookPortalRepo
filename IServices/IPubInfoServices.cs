038a282998171a7b8895054f57039a3ca3ecab1b
Ruchika Deore <ruchikadeore163@gmail.com>
2024-11-21 14:49:40 +0530

    Publisher and PubInfo Services

---
+++ IServices/IPubInfoServices.cs
@@ -0,0 +1,10 @@
+﻿using Boo_Store_Portal_Api.Dto;
+
+namespace Boo_Store_Portal_Api.IServices
+{
+    public interface IPubInfoServices
+    {
+        Task<ResponsePubInfoDto> UpdatePubInfoAsync(int id, UpdatePubInfoDto updatePubInfoDto);
+        Task<ResponsePubInfoDto> PatchPubInfoAsync(int id, PatchPubInfoDto patchPubInfoDto);
+    }
+}
